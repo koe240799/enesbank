2b5147726e9960e1db215654825d98e02a9974a4
start.vaadin.com <no-reply@vaadin.com>
2026-04-27 12:22:10 +0000

    Generated project

---
+++ src/main/java/at/spengergasse/views/costs/CostsView.java
@@ -0,0 +1,36 @@
+package at.spengergasse.views.costs;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Costs")
+@Route("delivery")
+@Menu(order = 2, icon = LineAwesomeIconUrl.WALLET_SOLID)
+public class CostsView extends VerticalLayout {
+
+    public CostsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
